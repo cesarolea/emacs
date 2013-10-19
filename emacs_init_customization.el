9c2ac229f3440d85b0fde3395338d097cbcb08ae
Cesar Olea <cesarolea@gmail.com>
2013-10-19 12:02:49 -0700

    Configured paredit mode

--- emacs_init_customization.el
+++ emacs_init_customization.el
@@ -180,3 +180,21 @@
 (require 'hippie-expand-slime)
 (add-hook 'slime-mode-hook 'set-up-slime-hippie-expand)
 (add-hook 'slime-repl-mode-hook 'set-up-slime-hippie-expand)
+
+; paredit
+(autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
+    (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+    (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+    (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+
+; paredit + slime, sitting on a tree...
+(add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+; Stop SLIME's REPL from grabbing DEL,
+; which is annoying when backspacing over a '('
+(defun override-slime-repl-bindings-with-paredit ()
+  (define-key slime-repl-mode-map
+	(read-kbd-macro paredit-backward-delete-key) nil))
+(add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
