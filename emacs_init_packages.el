f620b48b486aacdc13b783425268326e8bf1010e
Cesar Olea <cesarolea@gmail.com>
2014-04-24 09:24:31 -0700

    Added ace jump

--- emacs_init_packages.el
+++ emacs_init_packages.el
@@ -131,4 +131,6 @@
 (require 'sublimity-scroll)
 
 (require 'ace-jump-mode)
-(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-' SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-' C-u SPC") 'ace-jump-char-mode)
+(define-key global-map (kbd "C-' C-u C-u SPC") 'ace-jump-line-mode)
