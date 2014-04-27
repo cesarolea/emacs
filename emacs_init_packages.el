fabe1b33acaa8767d89ac0dbb297fd78c1263ca4
Cesar Olea <cesarolea@gmail.com>
2014-04-27 12:52:06 -0700

    Change back to C-c I can't find a good key binding for this

--- emacs_init_packages.el
+++ emacs_init_packages.el
@@ -131,6 +131,6 @@
 (require 'sublimity-scroll)
 
 (require 'ace-jump-mode)
-(define-key global-map (kbd "C-x SPC") 'ace-jump-mode)
-(define-key global-map (kbd "C-x C-u SPC") 'ace-jump-char-mode)
-(define-key global-map (kbd "C-x C-u C-u SPC") 'ace-jump-line-mode)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+(define-key global-map (kbd "C-c C-u SPC") 'ace-jump-char-mode)
+(define-key global-map (kbd "C-c C-u C-u SPC") 'ace-jump-line-mode)
