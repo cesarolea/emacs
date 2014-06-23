97841e5a13d3f1fd2c029d9fc9634a528d092ba4
Cesar Olea <cesarolea@gmail.com>
2014-06-23 07:30:22 -0700

    Disable custom theme in hostname specific files

--- emacs_init_minas.tirith.el
+++ emacs_init_minas.tirith.el
@@ -4,7 +4,7 @@
 (set-frame-size (selected-frame) 200 65)
 
 ;; set theme
-(load-theme 'moe-dark t)
+;; (load-theme 'moe-dark t)
 
 ;; set highlight line
 (set-face-background 'highlight-current-line-face "gray17")
