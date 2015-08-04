30de5c6c34f8a5788d1568317da8eb909717385f
Cesar Olea <cesarolea@gmail.com>
2015-08-04 08:56:11 -0700

    Disable magit prompts

--- emacs_init_use_package.el
+++ emacs_init_use_package.el
@@ -298,6 +298,7 @@
   :config (progn
             (setq magit-last-seen-setup-instructions "1.4.0")
             (global-set-key (kbd "<f10>") 'magit-status)
+            (add-to-list 'magit-no-confirm 'stage-all-changes)
             (setq magit-push-always-verify nil)
             (if (bound-and-true-p magit-auto-revert-mode)
                 (diminish 'magit-auto-revert-mode))))
