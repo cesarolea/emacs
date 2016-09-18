b0b67bab14f54ddb3ca75af58faf7168005f57ec
Cesar Olea <cesarolea@gmail.com>
2016-09-18 15:16:23 -0700

    Remove marmalade repo

--- lisp/custom.el
+++ lisp/custom.el
@@ -3,12 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("57f2bb1bc7a49065e5f02ca64bfbfe4dcbae99d48751c8092d8491eb3b7b03b4" default)))
- '(package-selected-packages
-   (quote
-    (web-mode use-package undo-tree swiper-helm sunrise-x-popviewer sunrise-x-modeline sunrise-x-mirror sunrise-x-loop sunrise-x-buttons smooth-scroll smartparens shrink-whitespace reveal-in-osx-finder rainbow-mode rainbow-delimiters powerline popwin popup-switcher peep-dired ox-reveal origami org-bullets move-text moe-theme magit js2-mode impatient-mode ido-vertical-mode highlight-symbol highlight-current-line helm-projectile helm-flx helm-descbinds helm-cider helm-ag git-timemachine flycheck flx-ido fireplace fill-column-indicator eyebrowse expand-region exec-path-from-shell dumb-jump diff-hl crux company-emoji clj-refactor buffer-flip bm beacon auto-highlight-symbol anzu ace-window ac-php))))
+ '(package-selected-packages (quote (use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
