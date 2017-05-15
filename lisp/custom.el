4740e485ad7016bc3750fbd0f08e521eea039efe
Cesar Olea <cesarolea@gmail.com>
2017-05-15 16:08:26 -0700

    lisp custom

--- lisp/custom.el
+++ lisp/custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (swiper dumb-jump web-mode use-package undo-tree terraform-mode swiper-helm smooth-scroll smartparens shrink-whitespace reveal-in-osx-finder restclient-helm rainbow-mode rainbow-delimiters powerline popwin popup-switcher ox-reveal origami org-bullets move-text moe-theme magit js2-mode impatient-mode ido-vertical-mode highlight-symbol highlight-current-line helm-projectile helm-flx helm-descbinds helm-cider helm-ag git-timemachine flycheck flx-ido fireplace eyebrowse expand-region exec-path-from-shell diff-hl crux company-restclient clj-refactor cider-hydra buffer-flip bm auto-highlight-symbol anzu ace-window))))
+    (highlight-indent-guides web-mode use-package undo-tree terraform-mode swiper-helm smooth-scroll smartparens shrink-whitespace reveal-in-osx-finder restclient-helm rainbow-mode rainbow-delimiters powerline popwin popup-switcher ox-reveal origami org-bullets move-text moe-theme magit js2-mode impatient-mode ido-vertical-mode highlight-symbol highlight-current-line helm-projectile helm-flx helm-descbinds helm-cider helm-ag git-timemachine flycheck flx-ido fireplace eyebrowse expand-region exec-path-from-shell dumb-jump diff-hl crux company-restclient clj-refactor cider-hydra buffer-flip bm auto-highlight-symbol anzu ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
