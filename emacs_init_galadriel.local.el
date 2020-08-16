62d8bc43186b2bfb5444700c46222cf63b624856
César Olea <cesaro@simnang.com>
2020-08-15 18:32:17 -0700

    Remove legacy configuration files

--- emacs_init_galadriel.local.el
+++
@@ -1,5 +0,0 @@
-;; galadriel.local config
-
-;; set frame size
-(set-frame-size (selected-frame) 140 45)
-(set-face-attribute 'default nil :height 140)
--- emacs_init_minas.tirith.el
+++
@@ -1,8 +0,0 @@
-; minas-tirith customization
-
-;; set frame position
-(set-frame-size (selected-frame) 200 65)
-
-
-;; refile target in minas
-(setq org-default-notes-file "/Volumes/Tomb/Sync/Org/refile.org")
