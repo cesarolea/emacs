e3df11ee16f34924d501b5f6037a5c134e243dbe
Cesar Olea <cesar.olea@cesar-olea.lan>
2022-12-03 02:01:58 -0700

    Switched to Vertico-Consult.

--- init.el
+++ init.el
@@ -15,6 +15,39 @@
 
 (setq straight-use-package-by-default t)
 
+;; Emacs specific stuff
+(use-package emacs
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+                  (replace-regexp-in-string
+                   "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+                   crm-separator)
+                  (car args))
+          (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator)
+
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+        '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  (setq read-extended-command-predicate
+         #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t)
+
+  ;; esc cancels all
+  (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+  (put 'narrow-to-region 'disabled nil))
+
+;; load org
 (use-package org
   :config
   (global-set-key "\C-cl" 'org-store-link)
@@ -248,5 +281,5 @@
                    ("\\subpparagraph{%s}" . "\\subparagraph*{%s}"))))
   )
 
+;; load config
 (org-babel-load-file (concat user-emacs-directory "README.org"))
-(put 'narrow-to-region 'disabled nil)
