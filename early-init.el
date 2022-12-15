1e0a5280808e3756860ff1aa424c6391893257eb
Cesar Olea <cesar.olea@cesar-olea.lan>
2022-12-15 15:54:19 -0700

    Setup macOS titlebar early

--- early-init.el
+++ early-init.el
@@ -1,5 +1,8 @@
 ;; disable package.el
 (setq package-enable-at-startup nil)
+
+;; no titlebar - rounded corners in macOS
+(add-to-list 'default-frame-alist '(undecorated-round . t))
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
